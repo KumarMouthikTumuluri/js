8ea52053cd781d6cf89c5d9e8c98dd3e5085b353
KumarMouthikTumuluri <67367625+KumarMouthikTumuluri@users.noreply.github.com>
2022-12-11 21:23:31 +0530

    Update list3.js

--- list folder/list3.js
+++ list folder/list3.js
@@ -4,25 +4,49 @@
 
 let List = require('../list')
 
-class Person{
-  constructor (name,gender){
-    this.name = name
-    this.gender = gender
+class Person {
+  constructor(name, gender) {
+    this.name   = name;
+    this.gender = gender;
   }
 }
 
-let names = ['John','Bon','Jovey','Heinricht','Heiny','Georg','Marmel','Ade','Ketuschu','Domio','Okkk','Rando','Exoo','Moboo','Yaby','Nobo','Karlsi','Mabondo','Numami','Esthabo','Minta','Haur']
-let gender = ['Male','Female','Transsexual Male','Transsexual Female','Metrosexual Male','Metrosexual Female','Other','None','Prefer Not to Say']
+class PersonList extends List {
 
-let ex3 = new List()
-
-ex3.displayGender = (gender) => console.log(ex3.dataStore.filter(a=>a.gender == gender));
-
-(()=>{
-  for(let i=0;i<10;i++){
-    ex3.add(new Person(names[Math.floor(Math.random()*names.length)],gender[Math.floor(Math.random()*gender.length)]))
+  displayByGender(gender) {
+    console.log(`Displaying only people who has the gender: ${gender}`);
+    this.dataStore.forEach((person, counter) => {
+      if(person.gender === gender){
+        console.log(`${counter + 1}.- ${person.name}`);
+      }
+    });
   }
-})()
+}
 
-ex3.print()
-ex3.displayGender('Male')
+console.log('\n\n### Excercise 3');
+
+const p1  = new Person('Jhon Doe', 'male');
+const p2  = new Person('Jane Doe', 'female');
+const p3  = new Person('Jose Doe', 'male');
+const p4  = new Person('Lady Doe', 'female');
+const p5  = new Person('Juan Colorado', 'male');
+const p6  = new Person('Maritza Doe', 'female');
+const p7  = new Person('Cris Ramirez', 'male');
+const p8  = new Person('More Ramirez', 'female');
+const p9  = new Person('Lenin Ramirez', 'male');
+const p10 = new Person('Jessy Doe', 'female');
+
+const personList = new PersonList();
+personList.append(p1);
+personList.append(p2);
+personList.append(p3);
+personList.append(p4);
+personList.append(p5);
+personList.append(p6);
+personList.append(p7);
+personList.append(p8);
+personList.append(p9);
+personList.insertAfter(p10,p3);
+personList.displayList();
+personList.displayByGender('male');
+personList.displayByGender('female');
