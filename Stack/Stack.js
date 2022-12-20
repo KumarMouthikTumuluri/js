4f693284d565d8f7f7813363d126ec41313b7ca3
KumarMouthikTumuluri <67367625+KumarMouthikTumuluri@users.noreply.github.com>
2022-12-20 12:30:33 +0530

    Update Stack.js

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,3 +1,64 @@
+########################################################################################################################################################################
+Description
+Pattern type: Last item In is the First item Out (LIFO)
+
+common operations you can perform on graphs:
+
+push: adds a new element to the stack
+pop: remove the last item pushed from the stack
+peek: returns the last item pushed to stack
+length: returns the size of the stack
+isEmpty: returns true if the stack has elements or false if it has no elements
+getStack: returns the data of the stack
+
+Example use case
+>Using the back and forward buttons in your browser
+>dfs search algorithm for graphs search
+>balanced arithmetic operations
+>convert infix to postfix arithmetic operations
+>JS engine uses a stack data structure to execute the algorithms
+
+
+(function (exports) {
+  const stack = (data = [], t = 0) => {
+    let dataStore = data
+    let top = t
+
+    const stackProto = {
+      push (element) {
+        dataStore[top++] = element
+      },
+      pop: () => dataStore.splice(--top, 1),
+      peek: () => dataStore[top - 1],
+      length: () => top,
+      isEmpty: () => top === 0,
+      getStack: () => dataStore
+    }
+
+    return Object.create(stackProto)
+  }
+
+  Object.assign(exports, {stack})
+}((typeof module.exports !== undefined) ? module.exports : window))
+
+/*(function (exports) {
+  const {stack} = require('./stack.module')
+  Object.assign(exports, {stack})
+}((typeof module.exports !== undefined) ? module.exports : window))*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################################################################################################################################
 // Chapter 4 - Stacks
 
 class Stack {
